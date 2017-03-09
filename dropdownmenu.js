c2fb0892ed0f7c7d629649de50be05ce88567dc3
RobertoBochet <robertobochet@gmail.com>
2017-03-09 02:49:24 +0100

    Disable hover for mobile version

--- dropdownmenu.js
+++ dropdownmenu.js
@@ -1,19 +1,19 @@
 $(function() {
-	$("nav.DropDownMenu > ul > li:has(ul)").click(function(){
-		if($(this).hasClass("click")){
+	$("nav.DropDownMenu > ul > li:has(ul)").click(function() {
+		if($(this).hasClass("click")) {
 			$(this).removeClass("click").removeClass("open");
 		} else {
 			$(this).parent().children("li").removeClass("click").removeClass("open");
 			$(this).addClass("click").addClass("open");
 		}
-	}).mouseenter(function(){
-		if(!$(this).parent().children("li").hasClass("click")) $(this).addClass("open");
+	}).mouseenter(function() {
+		if($(this).css("float") == "left") if(!$(this).parent().children("li").hasClass("click")) $(this).addClass("open");
 	}).mouseleave(function(){
-		if(!$(this).hasClass("click")) $(this).removeClass("open");
+		if($(this).css("float") == "left") if(!$(this).hasClass("click")) $(this).removeClass("open");
 	});
 	
 	/*Mobile version opening*/
-	$("body > header > div > svg").click(function(){
+	$("body > header > div > svg").click(function() {
 		$("body > header > div > nav").toggleClass("open");
 	});
 });
