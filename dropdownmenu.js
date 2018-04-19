0b5d4a01ad26fb474d95dd1f5ebb5c812e81bcf9
Roberto Bochet <robertobochet@gmail.com>
2018-04-19 02:13:02 +0200

    in-depth review

--- dropdownmenu.js
+++ dropdownmenu.js
@@ -1,19 +1,29 @@
-$(function() {
-	$("nav.DropDownMenu > ul > li:has(ul)").click(function() {
-		if($(this).hasClass("click")) {
-			$(this).removeClass("click").removeClass("open");
-		} else {
-			$(this).parent().children("li").removeClass("click").removeClass("open");
-			$(this).addClass("click").addClass("open");
-		}
-	}).mouseenter(function() {
-		if($(this).css("float") == "left") if(!$(this).parent().children("li").hasClass("click")) $(this).addClass("open");
-	}).mouseleave(function(){
-		if($(this).css("float") == "left") if(!$(this).hasClass("click")) $(this).removeClass("open");
-	});
-	
-	/*Mobile version opening*/
-	$("body > header > div > svg").click(function() {
-		$("body > header > div > nav").toggleClass("open");
-	});
-});
+(function($) {
+	$.fn.DropDownMenu = function(_button) {
+		var self = $(this);
+		var button = $(_button);
+		
+		self.children("ul").children("li:has(ul)").click(function() {
+			if($(this).hasClass("click")) {
+				$(this).removeClass("click").removeClass("open");
+			} else {
+				$(this).parent().children("li").removeClass("click").removeClass("open");
+				$(this).addClass("click").addClass("open");
+			}
+		}).mouseenter(function() {
+			if($(this).parent().css("flex-direction") == "row")
+				if(!$(this).parent().children("li").hasClass("click"))
+					$(this).addClass("open");
+		}).mouseleave(function(){
+			if($(this).parent().css("flex-direction") == "row")
+				if(!$(this).hasClass("click"))
+					$(this).removeClass("open");
+		});
+		
+		button.click(function() {
+			$(self).toggleClass("open");
+		});
+		
+		return this;
+	};
+}(jQuery));
