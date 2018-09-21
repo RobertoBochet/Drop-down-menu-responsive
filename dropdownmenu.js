51a00374c1bc358685d141abce3b3c05f1f1c481
Roberto Bochet <robertobochet@gmail.com>
2018-09-21 14:47:35 +0200

    Events now handled with 'addEventListener'

--- dropdownmenu.js
+++ dropdownmenu.js
@@ -14,7 +14,7 @@ class DropDownMenu
 		this.firstLayerElements = this.menu.querySelectorAll("ul > li");
 		
 		this.firstLayerElements.forEach((i)=>{
-			i.onclick = function() {
+			i.addEventListener("click", function() {
 				if(i.classList.contains("click")) {
 					i.classList.remove("click");
 					i.classList.remove("open");
@@ -27,47 +27,47 @@ class DropDownMenu
 						self.isFirstLayerOpened = true;
 					}
 				}
-			};
+			});
 			if(i.querySelector("ul") !== null) {
-				i.onmouseover = function() {
+				i.addEventListener("mouseover", function() {
 					if(self.isDesktop())
 						if(self.isFirstLayerOpened === false)
 							this.classList.add("open");
-				};
-				i.onmouseout = function() {
+				});
+				i.addEventListener("mouseout", function() {
 					if(self.isDesktop())
 						if(i.classList.contains("click") === false)
 							i.classList.remove("open");
-				};
+				});
 			}
 		});
 		
 		/*Closes the menu when any link is clicked. It's useful for Ajax pages*/
 		this.menu.querySelectorAll("a[href]").forEach((i)=>{
-			i.onclick = function(e) {
+			i.addEventListener("click", function(e) {
 				e.stopPropagation();
 				self.menu.classList.remove("open");
 				self.closeSubmenus();
-			};
+			});
 		});
 		
 		/*Handles the opening of the menu when you click on the button for mobile*/
-		this.button.onclick = (e)=>{
+		this.button.addEventListener("click", (e)=>{
 			e.stopPropagation();
 			if(this.menu.classList.contains("open"))
 				this.menu.classList.remove("open");
 			else
 				this.menu.classList.add("open");
-		};
+		});
 		
 		/*Allows the menu close when the user clicks outside the menu*/
-		this.menu.onclick = (e)=>{
+		this.menu.addEventListener("click", (e)=>{
 			e.stopPropagation();			
-		};
-		window.onclick = ()=>{
+		});
+		window.addEventListener("click", ()=>{
 			this.closeSubmenus();
 			this.menu.classList.remove("open");
-		};
+		});
 	}
 	
 	closeSubmenus()//Close all the submenus
